c7706458ea25f8def19cc1fd1973c419c4c26e70
blubinskyj <blubinskyj@gmail.com>
2022-03-16 16:59:25 +0200

    Add notification and sending email

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -2,11 +2,12 @@
 
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 async function closeAuction(auction) {
     const params = {
         TableName: 'AuctionsTable',
-        Key: {id: auction.id},
+        Key: { id: auction.id },
         UpdateExpression: 'set #status = :status',
         ExpressionAttributeValues: {
             ':status': 'CLOSED',
@@ -16,7 +17,30 @@ async function closeAuction(auction) {
         },
     };
 
-    return await dynamodb.update(params).promise();
+    await dynamodb.update(params).promise();
+
+    const { title, seller, highestBid } = auction;
+    const { amount, bidder } = highestBid;
+
+    const notifySeller = sqs.sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+            subject: 'Your item has been sold!',
+            recipient: seller,
+            body: `Congratulation! Your itme "${title}" has been sold for $${amount}.`,
+        }),
+    }).promise();
+
+    const notifyBidder = sqs.sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+            subject: 'You won an auction!',
+            recipient: bidder,
+            body: `Congratulation! You got yourself a "${title}" for $${amount}.`,
+        }),
+    }).promise();
+
+    return Promise.all([notifySeller, notifyBidder]);
 }
 
 module.exports = {closeAuction};
